ab5e1b733ef24ad6d39d63d2475153dd106c7879
abdoo-ahmed <abdoahmed8959@gmail.com>
2025-12-06 04:15:24 +0200

    Using APIS

---
+++ src/Features/Utils/api.ts
@@ -0,0 +1,16 @@
+const API_BASE_URL = "https://free-to-play-games-database.p.rapidapi.com";
+const MMORPG_API = `${API_BASE_URL}/api/games?category=mmorpg`;
+const SHOOTER_API = `${API_BASE_URL}/api/games?category=shooter`;
+const SAILING_API = `${API_BASE_URL}/api/games?category=sailing`;
+const PERMADEATH_API = `${API_BASE_URL}/api/games?category=permadeath`;
+const SUPERHERO_API = `${API_BASE_URL}/api/games?category=superhero`;
+const PIXEL_API = `${API_BASE_URL}/api/games?category=pixel`;
+
+export {
+  MMORPG_API,
+  SHOOTER_API,
+  SAILING_API,
+  PERMADEATH_API,
+  SUPERHERO_API,
+  PIXEL_API,
+};
