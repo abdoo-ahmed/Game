af389e4574aa78331fbaaa36d53ee66066351c98
abdoo-ahmed <abdoahmed8959@gmail.com>
2025-12-06 04:14:34 +0200

    Update Routes

--- src/Components/Layout/Layout.tsx
+++ src/Components/Layout/Layout.tsx
@@ -1,20 +1,23 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
-import Navbar from '../../Navbar/Navbar'
+import { Outlet } from "react-router-dom";
+import Navbar from "../../Navbar/Navbar";
 
 const Layout = () => {
-  return <>
-    
-    <div className="bg-[#272B30] w-full min-h-screen">
-      <div className="relative flex justify-center items-center">
-        <img className='w-full h-[210px]' src="../../../src/assets/Images/wraper.png" alt="wraper" />
-        <Navbar />
+  return (
+    <>
+      <div className="bg-[#272B30] w-full min-h-screen">
+        <div className="relative flex justify-center items-center">
+          <img
+            className="w-full h-[210px]"
+            src="../../../src/assets/Images/wraper.png"
+            alt="wraper"
+          />
+          <Navbar />
+        </div>
+
+        <Outlet />
       </div>
+    </>
+  );
+};
 
-      <Outlet />
-    </div>
-  
-  </>
-}
-
-export default Layout
+export default Layout;
--- src/Routes/Route.tsx
+++ src/Routes/Route.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Layout from "../Components/Layout/Layout";
 import { createBrowserRouter } from "react-router-dom";
 import MMORPG from "../Features/Components/MMORPG/MMORPG";
@@ -8,11 +7,18 @@ import PERMADEATH from "../Features/Components/PERMADEATH/PERMADEATH";
 import SUPERHERO from "../Features/Components/SUPERHERO/SUPERHERO";
 import PIXEL from "../Features/Components/PIXEL/PIXEL";
 
-export const router = createBrowserRouter([{ path: "/", element: <Layout /> , children:[
-    {path:"/MMORPG", element: <MMORPG />},
-    {path:"/SHOOTER", element: <SHOOTER/>},
-    {path:"/SAILING", element:<SAILING/>},
-    {path:"/PERMADEATH", element:<PERMADEATH/>},
-    {path:"/SUPERHERO", element:<SUPERHERO/>},
-    {path:"/PIXEL", element:<PIXEL/>}
-] }]);
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "/MMORPG", element: <MMORPG /> },
+      { path: "/", element: <MMORPG /> },
+      { path: "/SHOOTER", element: <SHOOTER /> },
+      { path: "/SAILING", element: <SAILING /> },
+      { path: "/PERMADEATH", element: <PERMADEATH /> },
+      { path: "/SUPERHERO", element: <SUPERHERO /> },
+      { path: "/PIXEL", element: <PIXEL /> },
+    ],
+  },
+]);
